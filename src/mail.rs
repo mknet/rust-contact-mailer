8193ddc31b1e2301012d3e73ad878a7da0644abc
Marcel Koch <kontakt@marcelkoch.net>
2019-08-30 09:03:58 +0200

    Setup basic functionality

---
+++ src/mail.rs
@@ -0,0 +1,41 @@
+use lettre::{SmtpClient, Transport};
+use lettre_email::{Email};
+use serde::{Deserialize, Serialize};
+use lettre::smtp::authentication::{Credentials, Mechanism};
+use lettre::smtp::ConnectionReuseParameters;
+
+#[derive(Serialize, Deserialize)]
+pub struct ContactMail {
+    pub sender_address: String,
+    pub message: String
+}
+
+pub fn send_contact_mail(mail_data: ContactMail) {
+    let email = Email::builder()
+        // Addresses can be specified by the tuple (email, alias)
+        .to(("kontakt@marcelkoch.net", "Marcel Koch"))
+        .from(mail_data.sender_address)
+        .subject("Kontaktformular")
+        .text(mail_data.message)
+        .build()
+        .unwrap();
+
+    // Open a local connection on port 25
+    let mut mailer = SmtpClient::new_simple("koch.kasserver.com").unwrap()
+        .smtp_utf8(true)
+        .credentials(Credentials::new("kontakt@marcelkoch.net".to_string(), "notvalid".to_string()))
+        .authentication_mechanism(Mechanism::Login)
+        .connection_reuse(ConnectionReuseParameters::ReuseUnlimited)
+        .transport();
+    // Send the email
+    let result = mailer.send(email.into());
+
+    if result.is_ok() {
+        println!("Email sent");
+    } else {
+        println!("Could not send email: {:?}", result);
+    }
+
+    mailer.close();
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,58 @@
+extern crate futures;
+extern crate gotham;
+extern crate hyper;
+extern crate serde_json;
+extern crate serde;
+
+use futures::{future, Future, Stream};
+use gotham::handler::{HandlerFuture, IntoHandlerError};
+use gotham::helpers::http::response::create_empty_response;
+use gotham::router::builder::{build_simple_router, DefineSingleRoute, DrawRoutes};
+use gotham::router::Router;
+use gotham::state::{FromState, State};
+use hyper::{Body, HeaderMap, Method, StatusCode, Uri, Version};
+
+mod mail;
+
+fn print_request_elements(state: &State) {
+    let method = Method::borrow_from(state);
+    let uri = Uri::borrow_from(state);
+    let http_version = Version::borrow_from(state);
+    let headers = HeaderMap::borrow_from(state);
+    println!("Method: {:?}", method);
+    println!("URI: {:?}", uri);
+    println!("HTTP Version: {:?}", http_version);
+    println!("Headers: {:?}", headers);
+}
+
+fn post_handler(mut state: State) -> Box<HandlerFuture> {
+    print_request_elements(&state);
+    let f = Body::take_from(&mut state)
+        .concat2()
+        .then(|full_body| match full_body {
+            Ok(valid_body) => {
+                let body_content = String::from_utf8(valid_body.to_vec()).unwrap();
+                println!("Body: {}", body_content);
+
+                let mail_data: mail::ContactMail = serde_json::from_str(body_content.as_str()).unwrap();
+
+                mail::send_contact_mail(mail_data);
+                let res = create_empty_response(&state, StatusCode::OK);
+                future::ok((state, res))
+            }
+            Err(e) => future::err((state, e.into_handler_error())),
+        });
+
+    Box::new(f)
+}
+
+fn router() -> Router {
+    build_simple_router(|route| {
+        route.post("/").to(post_handler);
+    })
+}
+
 fn main() {
-    println!("Hello, world!");
+    let addr = "0.0.0.0:7878";
+    gotham::start(addr, router())
 }
